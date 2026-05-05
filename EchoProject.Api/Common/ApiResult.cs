0f9feeb5767f43fb441cb2679d6fa99852362c99
CarlosAugustoP <capv2004@gmail.com>
2026-05-05 19:17:56 -0300

    Add stack trace to API RESULT

--- EchoProject.Api/Common/ApiResult.cs
+++ EchoProject.Api/Common/ApiResult.cs
@@ -6,10 +6,11 @@ namespace EchoProject.Api.Common
         public T? Data { get; private set; }
         public string? Error { get; private set; }
         public string? ErrorCode { get; private set; }
+        public string? StackTrace { get; private set; } = string.Empty;
         
         public DateTime Timestamp { get; private set; } = DateTime.UtcNow;
 
         public static ApiResult<T> Ok(T data) => new() { Success = true, Data = data };
-        public static ApiResult<T> Failure(string error, string errorCode) => new() { Success = false, Error = error, ErrorCode = errorCode };
+        public static ApiResult<T> Failure(string error, string errorCode, string? stackTrace = null) => new() { Success = false, Error = error, ErrorCode = errorCode, StackTrace = stackTrace ?? string.Empty };
     }
 }
