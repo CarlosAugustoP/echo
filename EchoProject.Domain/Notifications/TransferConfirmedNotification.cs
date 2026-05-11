ec5e68bfc3dbff927098503fbe30a93c1e3450c0
CarlosAugustoP <capv2004@gmail.com>
2026-05-11 10:39:33 -0300

    Fix

--- EchoProject.Domain/Notifications/TransferConfirmedNotification.cs
+++ EchoProject.Domain/Notifications/TransferConfirmedNotification.cs
@@ -11,7 +11,7 @@ namespace EchoProject.Domain.Notifications
         {
             const string donorMessage = "Sua doacao foi transferida com sucesso!";
             string donorDescription =
-                $"Agradecemos por sua generosidade. Sua doacao de {model.RealAmountEth.ToString("F2")} itens para o projeto {model.ProjectName} foi transferida para o Smart Contract e esta aguardando a transferencia para um fornecedor confiavel.";
+                $"Agradecemos por sua generosidade. Sua doacao de {model.RealAmountEth.ToString("F2")} ETH para o projeto {model.ProjectName} foi transferida para o Smart Contract e esta aguardando a transferencia para um fornecedor confiavel.";
             const string ngoMessage = "Nova doação recebida!";
             string ngoDescription =
                 $"O projeto {model.ProjectName} recebeu uma nova doacao de {model.RealAmountEth.ToString("F2")}. Realiza a transferência para o fornecedor e continue fortalecendo o living ledger!";
