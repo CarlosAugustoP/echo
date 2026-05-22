3f43e03cd21ac19f847fd00a6138ee6b27164939
CarlosAugustoP <capv2004@gmail.com>
2026-05-22 18:53:37 -0300

    Search

--- EchoProject.Application/Requests/Users/UpdateUserRequestValidator.cs
+++ EchoProject.Application/Requests/Users/UpdateUserRequestValidator.cs
@@ -9,23 +9,23 @@ namespace EchoProject.Application.Requests.Users
         {
             RuleFor(x => x.Name)
                 .MaximumLength(100)
-                .WithMessage("O nome deve ter no mÃ¡ximo 100 caracteres.")
+                .WithMessage("O nome deve ter no máximo 100 caracteres.")
                 .When(x => x.Name != null);
 
             RuleFor(x => x.Email)
                 .EmailAddress()
-                .WithMessage("Formato de e-mail invÃ¡lido.")
+                .WithMessage("Formato de e-mail inválido.")
                 .When(x => x.Email != null);
 
             RuleFor(x => x.Bio)
                 .MaximumLength(1000)
-                .WithMessage("A bio deve ter no mÃ¡ximo 1000 caracteres.")
+                .WithMessage("A bio deve ter no máximo 1000 caracteres.")
                 .When(x => x.Bio != null);
 
             RuleFor(x => x.ProfilePictureBase64)
                 .Must(ApplicationHelper.IsAValidBase64String!)
                 .When(x => !string.IsNullOrEmpty(x.ProfilePictureBase64))
-                .WithMessage("A foto de perfil deve ser uma string Base64 vÃ¡lida.");
+                .WithMessage("A foto de perfil deve ser uma string Base64 válida.");
         }
     }
 }
